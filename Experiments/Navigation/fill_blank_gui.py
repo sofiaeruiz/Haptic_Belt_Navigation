f181ee1d14afa186ae1705a524399524075a8d53
sofiaeruiz <sofia_e_ruiz@brown.edu>
2025-06-05 15:25:41 -0700

    Update fill_blank_gui.py
    Adding new questions for HapticBeltExperiment 2

--- Experiments/Navigation/fill_blank_gui.py
+++ Experiments/Navigation/fill_blank_gui.py
@@ -29,9 +29,13 @@ def main_fill_in(subID, experiment_type):
     strategy_text = tk.Text(root, height=4, width=40)
     strategy_text.grid(row=0, column=1, padx=10, pady=10)
 
-    tk.Label(root, text="Is there anything else you noticed about the \nexperiment that you would like to share?").grid(row=1, column=0, padx=10, pady=10)
+    tk.Label(root, text="Did you notice any change to the vibrations when \ngetting closer or farther away from the target? \nIf so, did this help you navigate to the target \nwaypoint more efficiently?").grid(row=1, column=0, padx=10, pady=10)
+    change_vibrations_text = tk.Text(root, height=4, width=40)
+    change_vibrations_text.grid(row=1, column=1, padx=10, pady=10)
+
+    tk.Label(root, text="Is there anything else you noticed about the \nexperiment that you would like to share?").grid(row=2, column=0, padx=10, pady=10)
     other_observations_text = tk.Text(root, height=4, width=40)
-    other_observations_text.grid(row=1, column=1, padx=10, pady=10)
+    other_observations_text.grid(row=2, column=1, padx=10, pady=10)
 
     # Submit button
     submit_btn = tk.Button(root, text="Submit", command=lambda: submit_fill_in(subID, experiment_type, strategy_text, other_observations_text, root))
